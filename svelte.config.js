f9f0b9fc368df3ca550b547d7a61f967a567c506
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-03-26 14:29:26 +0300

    add own adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from 'svelte-adapter-static-digitalocean';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		adapter: adapter(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
